181f7ce84457780a03d6ddedc6f4443133e3c271
Snehin Kukreja <snehin.kukreja@gmail.com>
2020-06-24 18:09:36 +0200

    Update piano.m

--- piano.m
+++ piano.m
@@ -1,15 +1,11 @@
 function note = piano(f, t_max)
-
-fs = 44100;
-dt = 1/fs;
-t = 0:dt:t_max;
-
-
-w = 2*pi*f;
-
-eg = 0.6*sin(w*t) .* exp(-0.0015*w*t);
-eg = eg + 0.4*sin(2*w*t) .* exp(-0.0015*w*t);
-eg = eg + eg.*eg.*eg;
-eg = eg .*( 1 +16*t.*exp(-6*t));
-
-note = eg;
+    % f - some input frequency
+    % t_max - the duration of the note
+    fs = 44100; %fixed sample rate
+    dt = 1/fs;
+    t = 0:dt:t_max;
+    w = 2*pi*f;
+    note = 0.6*sin(w*t) + 0.4*sin(2*w*t) + 0.03*sin(4*w*t); %first three freq added
+    note = note + 0.02*sin(5*w*t) + 0.025*sin(7*w*t); % last two freq added
+    note = note.*exp(-0.002*w*t); % multiply by decaying factor
+end
